00936386ee9cc367479e85a41324d5d7e1125439
Colin Bruner <colin.d.bruner@gmail.com>
2020-02-17 23:19:23 -0500

    update

--- bootstrap/salt.sh
+++ bootstrap/salt.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-SALT_REPO="https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el8.noarch.rpm"
-INSTALL_PKGS="git salt-minion"
-SALT_MINION="salt-minion"
+function install_salt_minion_raspbian() {
+    REPO="deb http://archive.raspbian.org/raspbian/ stretch main"
+    if [[ ! $(grep "$REPO" /etc/apt/sources.list) ]]; then
+        echo "$REPO" >> /etc/apt/sources.list
+    fi
+    apt-get update -y
+    apt-get install -y salt-common/stable salt-minion/stable
+}
 
-function install_salt_minion() {
+function install_salt_minion_centos() {
+    SALT_REPO="https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el8.noarch.rpm"
+    INSTALL_PKGS="git salt-minion"
+    SALT_MINION="salt-minion"
+
+    echo "Installing Master/Minion..."
     yum install -y $SALT_REPO && yum install -y $INSTALL_PKGS
     echo "Master/Minion installed successfully."
     systemctl start $SALT_MINION && systemctl enable $SALT_MINION
     echo "Minion started and enabled successfully."
+
+}
+
+function install_salt_minion() {
+    if [[ $(uname) == "Linux" ]]; then
+        distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+        if [[ $distro =~ "CentOS"* ]]; then
+            # Install Salt on CentOS
+            install_salt_minion_centos
+        elif [[ $distro =~ "Raspbian"* ]]; then
+            # Install Salt on Raspberry Pi's
+            install_salt_minion_raspbian
+        fi
+    fi
 }
 
 function bootstrap_salt() {
